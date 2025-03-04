3e855cc9c8ba6098a1c3b8dd372dda38ae00000a
Fatima Bilal <95558943+faticoco@users.noreply.github.com>
2025-03-04 23:24:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ import os
 app = Flask(__name__)
 
 items = [
-    {"id": 1, "name": "Item 1", "description": "This is item 1"},
-    {"id": 2, "name": "Item 2", "description": "This is item 2"}
+    {"id": 1, "name": "Item 1", "description": "This is item 1."},
+    {"id": 2, "name": "Item 2", "description": "This is item 2."}
 ]
 
 
